3713558ae159b43dbc313d1b2d8f87237d1a5e84
Taseen Waseq <76471599+taseenw@users.noreply.github.com>
2022-08-24 02:27:12 -0400

    Time: 90 ms (38.84%), Space: 13.8 MB (56.38%) - LeetHub

--- 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.py
+++ 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.py
@@ -3,7 +3,7 @@ class Solution:
         x=0
         
         for s in operations:
-            if "+" in s:
+            if "++" in s:
                 x+=1
             else:
                 x-=1
