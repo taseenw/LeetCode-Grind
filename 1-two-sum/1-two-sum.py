53a41ec1d61927dac164f379793487827b8ab0fe
Taseen Waseq <76471599+taseenw@users.noreply.github.com>
2022-08-26 04:44:17 -0400

    Time: 59 ms (97.97%), Space: 15.2 MB (50.22%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -1,6 +1,11 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for x in range(len(nums)):
-            for y in range(x+1,len(nums)):
-                if nums[x]+nums[y]==target:
-                        return [x,y]
+        hmap={}
+
+        for i, n in enumerate(nums):
+            diff = target - n
+            if diff in hmap:
+                return [hmap[diff], i]
+            hmap[n] = i
+        return
+    
