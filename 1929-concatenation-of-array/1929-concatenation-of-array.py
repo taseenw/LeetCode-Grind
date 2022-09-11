c69a9559ec4a69bd21904c644447ec2ed052c30c
Taseen Waseq <76471599+taseenw@users.noreply.github.com>
2022-09-11 02:43:25 -0400

    Time: 196 ms (5.14%), Space: 13.5 MB (91.51%) - LeetHub

--- 1929-concatenation-of-array/1929-concatenation-of-array.py
+++ 1929-concatenation-of-array/1929-concatenation-of-array.py
@@ -1,9 +1,15 @@
 class Solution(object):
     def getConcatenation(self, nums):
         n=len(nums)
+        hmap={}
+        ans=[]
         
-        for x in range (n):
-            nums.append(nums[x])
+        for x in range(n):
+            hmap[x]=nums[x]
+            ans.append(hmap[x])
+            
+        for x in range(n):
+            ans.append(hmap[x])
         
-        return nums
+        return ans
         
